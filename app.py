1b317e937366a3ffe8a69bd352152e0fd06a70c5
Elizedles <163445849+Elizedles@users.noreply.github.com>
2026-03-26 21:39:38 -0700

    Sprint 5 - Optimización en el manejo de datos de entrada
    Optimize input data for price prediction

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ calidad = int(input("Ingrese calidad (1-10): "))
 area = float(input("Ingrese área habitable: "))
 habitaciones = int(input("Ingrese número de habitaciones: "))
 banos = int(input("Ingrese número de baños: "))
-garaje = int(input("Ingrese número de garajes "))
+garaje = int(input("Ingrese número de garajes: "))
 
-precio = predecir_precio(calidad, area, habitaciones, banos, garaje)
+# Optimización (agrupamos datos)
+datos = [calidad, area, habitaciones, banos, garaje]
+
+precio = predecir_precio(*datos)
 
 print("\nPrecio estimado: $", precio)

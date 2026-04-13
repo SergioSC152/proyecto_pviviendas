f5eabfae6b488362157b5375bd08b8dd6da9833c
SergioSC152 <smanuelsanchez@ucundinamarca.edu.co>
2026-04-12 23:05:11 -0700

    Commit 9 Sprint3: Implementación de fetch y pruebas de conexión con backend
    En este commit se desarrolló la base del backend utilizando Flask.
    
    Se creó el endpoint /predecir encargado de recibir datos en formato JSON
    
    y procesarlos mediante el modelo previamente entrenado
    
    Creación del archivo app.py
    
    Configuración del servidor Flask
    
    Definición de la ruta /predecir
    
    Integración de la función predecir_precio
    
    Pruebas locales del backend
    
    Backend funcional de forma independiente
    
    Modelo cargando correctamente
    
    Sin conexión aún con frontend

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from model import predecir_precio
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/predecir", methods=["POST"])
+def predecir_api():   
+
+    datos = request.json
+
+    precio = predecir_precio(
+        datos["calidad"],
+        datos["area"],
+        datos["habitaciones"],
+        datos["banos"],
+        datos["garaje"]
+    )
+
+    return jsonify({"precio": precio})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+
